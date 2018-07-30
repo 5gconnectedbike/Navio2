418830be92434590e296a8f2044629c2877ce412
Ahmet Akgul <ahmet.akgul@ericsson.com>
2018-07-30 12:34:03 -0400

    Add barometer code

--- Python/ubi_realdata.py
+++ Python/ubi_realdata.py
@@ -1,5 +1,6 @@
 import navio.util
 import navio.ublox
+import navio.ms5611
 import time
 import requests
 import math
@@ -16,12 +17,14 @@ TEMPERATURE = 0
 HUMIDITY = 0
 
 def update_gps(GPSdict):
-    global LATITUDE, LONGITUDE, TEMPERATURE, HUMIDITY
+    global LATITUDE, LONGITUDE
     LATITUDE = int(GPSdict['Latitude'])
     LONGITUDE = int(GPSdict['Longitude'])
-    TEMPERATURE = int(GPSdict['height'])
-    HUMIDITY = int(GPSdict['hAcc'])
 
+def update_baro(temperature, humidity):
+    global TEMPERATURE, HUMIDITY
+    TEMPERATURE = temperature
+    HUMIDITY = humidity
 
 def get_gps():
     return (LATITUDE, LONGITUDE, TEMPERATURE, HUMIDITY)
@@ -76,6 +79,12 @@ def main():
 
 
 if __name__ == '__main__':
+
+    navio.util.check_apm()
+
+    baro = navio.ms5611.MS5611()
+    baro.initialize()
+
     ubl = navio.ublox.UBlox("spi:0.0", baudrate=5000000, timeout=2)
 
     ubl.configure_poll_port()
@@ -112,6 +121,19 @@ if __name__ == '__main__':
     #ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_DGPS, 5)
 
     while (True):
+        baro.refreshPressure()
+        time.sleep(0.01) # Waiting for pressure data ready 10ms
+        baro.readPressure()
+
+        baro.refreshTemperature()
+        time.sleep(0.01) # Waiting for temperature data ready 10ms
+        baro.readTemperature()
+
+        baro.calculatePressureAndTemperature()
+        update_baro(baro.TEMP, baro.PRES)
+
+        print("Temperature(C): %.6f" % (baro.TEMP), "Pressure(millibar): %.6f" % (baro.PRES))
+
         msg = ubl.receive_message()
 
         if msg is None:
