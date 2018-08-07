bc43f62c2dccbf4f19f7cbfb41da8301ecab7eb7
Ahmet Akgul <ahmet.akgul@ericsson.com>
2018-08-07 16:18:58 -0400

    m

--- Python/multiple_thread.py
+++ Python/multiple_thread.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process
 import os
+import time
 
 def info(title):
     print(title)
@@ -9,12 +10,25 @@ def info(title):
     
 def foo(name):
     info('function f')
-    print('hello', name)
+    count = 0
+    while(True):
+        print('Thread {}, count: {}'.format(name, count))
+        count+=1
+        time.sleep(1)
+        
+    # print('hello', name)
+def boo(name):
+    info('function b')
+    count = 0
+    while(True):
+        print('Thread {}, count: {}'.format(name, count))
+        count+=1
+        time.sleep(1)
     
 if __name__ == '__main__':
     info('main line')
     p = Process(target=foo, args=('bob', ))
-    j = Process(target=foo, args=('sarah', ))
+    j = Process(target=boo, args=('sarah', ))
     j.start()
     p.start()
     p.join()
