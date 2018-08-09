ff20bee926cc74a1fd2d090e66145b6e1c4fbeed
Ahmet Akgul <ahmet.akgul@ericsson.com>
2018-08-09 15:52:26 -0400

    m

--- Python/ubi_realdata.py
+++ Python/ubi_realdata.py
@@ -140,7 +140,7 @@ class Ubidots:
 
     def build_payload(self, variable_1, variable_2, variable_3, variable_4, variable_5, variable_6, variable_7, variable_8):
         lat, lng = Ubidots.get_gps()
-        temp_value, pressure_value = Ubidots.get_baro()
+        temp_value, pressure_value = self.get_baro()
         speed, heading = Ubidots.get_speed()
         m9a, m9g, m9m = Ubidots.get_accel()
         # print('lat: {} lng: {} temp_value: {} pressure value: {} m9m:{} {} {}'.format(lat, lng, temp_value, pressure_value, m9m[0], m9m[1], m9m[2]))
